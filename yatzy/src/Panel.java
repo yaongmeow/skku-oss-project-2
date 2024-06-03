feed76929da96b78ef24b6f653212c60bce9d7ba
Suyeong <hsy3539@naver.com>
2024-06-04 02:30:14 +0900

    panel 생성

---
+++ yatzy/src/Panel.java
@@ -0,0 +1,11 @@
+import javax.swing.*;
+import javax.swing.border.EmptyBorder;
+import java.awt.*;
+
+public class Panel extends JPanel {
+    public Panel() {
+        this.setBackground(new Color(255, 255, 255));
+        this.setBorder(new EmptyBorder(5, 5, 5, 5));
+        this.setLayout(null);
+    }
+}
--- yatzy/src/Screen.java
+++ yatzy/src/Screen.java
@@ -1,9 +1,19 @@
 import javax.swing.*;
+import javax.swing.border.EmptyBorder;
 import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.util.List;
 
 public class Screen extends JFrame implements ActionListener {
+    private JPanel panel;
+
+    public Screen() throws HeadlessException {
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        setBounds(100, 100, 100, 100);
+        setResizable(false);
+        Panel panel = new Panel();
+    }
 
     void floatScreen() {
         EventQueue.invokeLater(new Runnable() {
@@ -18,6 +28,8 @@ public class Screen extends JFrame implements ActionListener {
             }
         });
     }
+
+
 
     @Override
     public void actionPerformed(ActionEvent e) {
